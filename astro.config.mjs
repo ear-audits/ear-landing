013ab2102a988f0c203deab7cbc7aaa7b1c11167
damian5 <damian.lingua1@gmail.com>
2025-12-05 23:05:18 -0300

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    prefetch: {
+        prefetchAll: true,
+        defaultStrategy: 'viewport'
+    },
+    vite: {
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        'lucide': ['lucide-astro']
+                    }
+                }
+            }
+        }
+    }
+});
